37c068c7c8f3b1b618dac5064a99cbe153e606b3
Ayush9452 <120298589+Ayush9452@users.noreply.github.com>
2023-09-23 01:18:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function updatemap()
+{
+    console.log("calling....");
+    fetch('/data.json')
+    .then(response => response.json())
+    .then(rsp => {
+        // console.log(rsp.data);
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+
+            cases = element.infected;
+            if(cases >= 255)
+            {
+                color = 'rgb(255,0,0)'
+            }
+            else
+            {
+                color = `rgb(${cases},0,0)`
+            }
+
+            //place in map
+            marker = new mapboxgl.Marker({
+                draggable: false,
+                color: color
+                })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+        });
+
+    })
+}
+
+let interval = 2000
+setInterval(updatemap,interval);
